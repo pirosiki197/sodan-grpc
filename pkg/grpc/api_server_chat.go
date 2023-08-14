631c53f2e65d691ecf28d2112de8977841b6f88f
pirosiki197 <lingmukangtai825@gmail.com>
2023-08-14 20:17:56 +0900

    tagでsodanを抽出できるようにした

--- pkg/grpc/api_server_chat.go
+++ pkg/grpc/api_server_chat.go
@@ -23,10 +23,11 @@ var (
 )
 
 func (s *server) CreateReply(ctx context.Context, req *connect.Request[apiv1.CreateReplyRequest]) (*connect.Response[apiv1.CreateReplyResponse], error) {
+	s.logger.Info("CreateReply", "req", req.Msg)
 	reply := &dto.ReplyDto{
-		Text:      req.Msg.GetText(),
-		CreaterID: req.Msg.GetCreaterId(),
-		SodanID:   uint(req.Msg.GetSodanId()),
+		Text:      req.Msg.Text,
+		CreaterID: req.Msg.CreaterId,
+		SodanID:   uint(req.Msg.SodanId),
 	}
 
 	id, err := s.replyService.CreateReply(reply)
@@ -45,7 +46,7 @@ func (s *server) CreateReply(ctx context.Context, req *connect.Request[apiv1.Cre
 }
 
 func (s *server) GetReply(ctx context.Context, req *connect.Request[apiv1.GetReplyRequest]) (*connect.Response[apiv1.GetReplyResponse], error) {
-	id := req.Msg.GetId()
+	id := req.Msg.Id
 	reply, err := s.replyService.FindByID(uint(id))
 	if err != nil {
 		return nil, err
@@ -63,6 +64,7 @@ func (s *server) GetReply(ctx context.Context, req *connect.Request[apiv1.GetRep
 }
 
 func (s *server) GetReplies(ctx context.Context, req *connect.Request[apiv1.GetRepliesRequest]) (*connect.Response[apiv1.GetRepliesResponse], error) {
+	s.logger.Info("GetReplies", "req", req.Msg)
 	sodanID := req.Msg.SodanId
 	replies, err := s.replyService.FindBySodanID(uint(sodanID))
 	if err != nil {
@@ -83,6 +85,7 @@ func (s *server) GetReplies(ctx context.Context, req *connect.Request[apiv1.GetR
 }
 
 func (s *server) SubscribeSodan(ctx context.Context, req *connect.Request[apiv1.SubscribeSodanRequest], stream *connect.ServerStream[apiv1.SubscribeSodanResponse]) error {
+	s.logger.Info("SubscribeSodan", "req", req.Msg)
 	sodanId := req.Msg.Id
 	ch := make(chan newReplyInfo)
 	appendCh(ch)
