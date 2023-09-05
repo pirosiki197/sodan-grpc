b5f81467009a5d16583b52389e920bddf176f729
pirosiki197 <lingmukangtai825@gmail.com>
2023-09-05 19:32:37 +0900

    fix

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -5,8 +5,8 @@ import CreateSodanPage from './pages/CreateSodanPage.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomePage },
-  { path: '/sodan/:id', name: 'sodan', component: SodanChatPage, props: true},
-  { path: '/sodan/create', name: 'create', component: CreateSodanPage}
+  { path: '/sodan/:id', name: 'sodan', component: SodanChatPage, props: true },
+  { path: '/sodan/create', name: 'create', component: CreateSodanPage }
 ]
 
 const router = createRouter({
